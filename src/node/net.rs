1fb182ba33e922b3e28a835e2acb8079fd4ba10a
mm <mm@matterlabs.dev>
2024-10-29 13:53:12 +0100

    use the chain_id in net_version too

--- src/node/net.rs
+++ src/node/net.rs
@@ -10,7 +10,12 @@ impl<S: ForkSource + std::fmt::Debug + Clone + Send + Sync + 'static> NetNamespa
     for InMemoryNode<S>
 {
     fn net_version(&self) -> Result<String> {
-        Ok(TEST_NODE_NETWORK_ID.to_string())
+        let chain_id = self
+            .get_inner()
+            .read()
+            .map(|reader| reader.config.chain_id.unwrap_or(TEST_NODE_NETWORK_ID))
+            .expect("Failed to get lock");
+        Ok(chain_id.to_string())
     }
 
     fn net_peer_count(&self) -> Result<U256> {
